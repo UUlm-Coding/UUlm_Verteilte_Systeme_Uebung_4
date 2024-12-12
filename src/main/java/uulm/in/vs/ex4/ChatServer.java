46bddba9b5e02147abe8f3a08b8be9f7a17ba7bb
jglumanda <94634381+JGlumanda@users.noreply.github.com>
2024-12-12 10:30:27 +0100

    Implementation of A1

--- src/main/java/uulm/in/vs/ex4/ChatServer.java
+++ src/main/java/uulm/in/vs/ex4/ChatServer.java
@@ -2,9 +2,11 @@ package uulm.in.vs.ex4;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 
 import java.io.IOException;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ChatServer {
@@ -13,15 +15,33 @@ public class ChatServer {
     public static class ChatService extends ChatGrpc.ChatImplBase {
         @Override
         public void login(LoginRequest request, StreamObserver<LoginResponse> responseObserver) {
-            // TODO
+            String username = request.getUsername();
+            if (users.containsKey(username)) {
+                responseObserver.onError(Status.FAILED_PRECONDITION
+                        .withDescription("Username already taken")
+                        .asRuntimeException());
+                return;
+            }
+            String sessionID = UUID.randomUUID().toString();
+            users.put(username, sessionID);
+            LoginResponse response = LoginResponse.newBuilder().setSessionID(sessionID).build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
         }
 
         @Override
         public void logout(LogoutRequest request, StreamObserver<LogoutResponse> responseObserver) {
-            // TODO
+            String username = request.getUsername();
+            if (!users.containsKey(username)) {
+                responseObserver.onError(Status.NOT_FOUND
+                        .withDescription("SessionID not found")
+                        .asRuntimeException());
+                return;
+            }
+            users.remove(username);
+            responseObserver.onNext(LogoutResponse.newBuilder().build());
+            responseObserver.onCompleted();
         }
-
-        // TODO
     }
 
     public static void main(String[] args) {
